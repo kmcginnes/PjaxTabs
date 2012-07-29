d5e6c09e52cc8ed8ac063bbe1e54c6940bedf46f
Kris McGinnes <kmcginnes@me.com>
2012-07-29 17:08:25 -0500

    Initial commit

---
+++ PjaxTabExample/Controllers/FooController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace PjaxTabExample.Controllers
+{
+    public class FooController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.SelectedTab = "Bar";
+            return View();
+        }
+
+        public ActionResult Bar()
+        {
+            if(Request.Headers["X-PJAX"] != null)
+                return PartialView();
+            ViewBag.SelectedTab = "Bar";
+            return View("Index");
+        }
+
+        public ActionResult Baz()
+        {
+            if (Request.Headers["X-PJAX"] != null)
+                return PartialView();
+            ViewBag.SelectedTab = "Baz";
+            return View("Index");
+        }
+    }
+}
---
+++ PjaxTabExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PjaxTabExample.MvcApplication" Language="C#" %>
